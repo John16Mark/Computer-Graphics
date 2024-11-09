8e94687590e62334e264ef27228aab7ae3590b9d
John16Mark <jlma-113@hotmail.com>
2024-11-08 20:40:04 -0600

    Librería SDL general

--- Práctica 3/compilar.bat
+++ Práctica 3/compilar.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-gcc -I src/include -L src/lib -o bin/main src/main.c src/structs/*.c -lmingw32 -lSDL2main -lSDL2
+gcc -I ../include -L ../lib -o bin/main src/main.c src/structs/*.c -lmingw32 -lSDL2main -lSDL2
